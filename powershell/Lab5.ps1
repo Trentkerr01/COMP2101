df9e91d5288744c290628c10bc691742e8cd0bee
Trentkerr01 <90343824+Trentkerr01@users.noreply.github.com>
2021-12-05 12:24:33 -0500

    Create Lab5.ps1

---
+++ powershell/Lab5.ps1
@@ -0,0 +1,27 @@
+﻿param ( [parameter(mandatory=$true)][string]$system,
+        [parameter(mandatory=$true)][string]$Disk,
+        [parameter(mandatory=$true)][string]$Network )
+
+
+Write-Output "The System variable contains '$system'."
+Write-Output "The Disk variable contains '$Disk'."
+Write-Output "The Network Variable contains '$Network'."
+
+if ( $system -like "True" ) {
+get-cpuinfo
+OperatingSystemInfo
+RamSummary
+VideoCard
+}
+else { Write-Output "You have not chose to see System information" }
+
+if ( $Disk -like "True" ) {
+get-mydisks
+PhysicalDisk
+}
+else { Write-Output "You have not chose to see Disk information" }
+
+if ( $Network -like "True" ) {
+NetworkAdapter
+}
+else { Write-Output "You have not chose to see Network information" }
